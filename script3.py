2b6e76f4225e8524b9a9b56804d8cad35687228a
unknown <Minskoleopc>
2024-01-04 21:54:36 +0530

    added

---
+++ script3.py
@@ -0,0 +1,128 @@
+# x = 10 
+# print(x)
+# x = 900
+# print(x)
+
+# s = 10 
+# t = 5
+
+# print(s+t)
+# print(s-t)
+# print(s*t)
+# print(s/t)
+# print(s%t)
+
+
+# q = 8
+# r = 4
+
+# print(q+r)
+# print(q-r)
+# print(q*r)
+# print(q/r)
+# print(q/r)
+
+
+# def Calculator(x,y):
+#     print(x+y)
+#     print(x-y)
+#     print(x*y)
+#     print(x/y)
+#     print(x%y)
+# Calculator(12,4)
+# Calculator(9,3)
+
+# def addA():
+#     print(9+9)
+# addA()
+# addA()
+# addA()
+# addA()
+
+# def addB(x,y):
+#     print(x+y)
+# addB(12,4)
+# addB(12,1)
+# addB(2,4)
+# addB(11,4)
+
+# def addC(x,y):
+#     return x + y
+# q2 = addC(12,4)
+# print(q2)
+# print(q2+q2)
+# print(q2/4)
+# print(q2 * 0.10)
+
+
+# type 
+
+# introvert    # extrovert
+# calm          loud
+# less outing   more outing
+# less social   more social
+
+# Human
+# Properties - age  ,weight , height ,color, gender
+# Method - sleep() , walk()
+
+# Vehicle 
+# Properties - Color , type , regNo , city , model , company
+# Method - start()  stop()
+
+#Bank acc
+#Properties - accNo , accName , IFSC code , bal,  branchName
+#Method - withdrawl() , deposit()
+
+
+# int
+x = 10 
+print(x)
+print(type(x))
+# 89,-90,0
+
+# float
+y = 100.7
+print(y)
+print(type(y))
+# 23.4 ,-5.6 ,0.0
+
+# boolean 
+z = True 
+print(z)
+print(type(z))
+# True or False
+
+#string 
+name = "chinmay"
+print(name)
+print(type(name))
+#"A","a","312","DaA@@","123123","4324REWREWR"," "
+
+
+# comparison operator
+#entity < entity  # True or False
+# < , > , <= , >= ,!= , ==
+
+print(3 == 3) # True
+print(3 != 3) # False
+print(3 >= 2) # True
+print(3 <= 2) # False
+print(3 > 4) # False
+print(3 < 5) # True
+print(3 >= 3) #True
+print(3 <= 3) # True
+print(3 == 7) # False
+print(3 != 7) # True
+
+
+
+
+
+
+
+
+
+
+
+
