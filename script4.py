1a38c8b629ed8bed294641573510a0c18944ffea
unknown <Minskoleopc>
2024-01-05 21:55:33 +0530

    added

---
+++ script4.py
@@ -0,0 +1,95 @@
+
+# type 
+# introvert       # extrovert
+# calm            # loud
+# less social     # more social
+# less outing     # more outing
+
+# Human -
+# Properties - color , gender , height , weight 
+# Method  - walk() , talk()
+
+# Vehicle
+# Properties - color , modelNumber , type , companyName
+# Method - start() , stop()
+
+# Bankacc -- balance , accNo , IFSC code , accName
+# Properties - withdrawl() , deposit()
+
+x = 10
+print(x)
+print(type(x))
+# 10,-10,0
+
+y = 10.5
+print(y)
+print(type(y))
+# 12.4 , 45.5 , 0.0
+
+z = True
+print(z)
+print(type(z))
+# True or False
+
+c = "chinmay"
+print(c)
+print(type(c))
+#'A','1','@','234432',"@123213"," "
+
+# comparison 
+# enity < entity =====> Boolean ===> True or False
+# < , > , <= , >= , == , != 
+print(4 < 5)  # True
+print(4 > 3)  # True
+print(4 >= 3) # True
+print(4 <= 3) # False
+print(4 == 3) #False
+print(4 != 3) # True
+print(4 == 3) # False
+print(4 >= 4) # True
+print(4 <= 4) # True
+
+
+# logical operator 
+
+
+# and 
+#True  and True   =====> True
+#False and True   =====> False
+#True  and False  =====> False
+#False and False  =====> False
+
+print(22 == 22 and 33 == 33)
+print(22 != 22 and 33 == 33)
+print(22 == 22 and 33 != 33)
+print(22 != 22 and 33 != 33)
+
+# or 
+
+#True  or True   =====> True
+#False or True   =====> True
+#True  or False  =====> True
+#False or False  =====> False
+
+
+print(22 == 22 or 33 == 33)
+print(22 != 22 or 33 == 33)
+print(22 == 22 or 33 != 33)
+print(22 != 22 or 33 != 33)
+
+# not
+#not True --- False
+#not False -- True
+
+print(not 23 == 23)
+print(not 23 == 24)
+
+
+
+
+
+
+
+
+
+
