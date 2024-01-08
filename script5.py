f5396b3bd59b4475ae92fd68dbfe405876ed0331
unknown <Minskoleopc>
2024-01-08 21:57:48 +0530

    added

---
+++ script5.py
@@ -0,0 +1,95 @@
+# conditional statement
+
+# one input and multiple outcomes
+
+
+# concert 
+
+# numT > 0 and numT <= 5     ----->  10 % discount
+# numT > 5 and numT <= 10    ----->  20 % discount
+# numT > 10                  ----->  30 % discount 
+
+# if condition:
+#     print("hello")
+
+numT  = 17
+if numT > 0 and numT <= 5:
+    print("10 % discount")
+if numT > 5 and numT <= 10:
+    print("20 % discount")
+if numT > 10:
+    print("30 % discount")
+
+
+# program 2
+numT = -18
+if numT > 0 and numT <= 5:
+    print("10 % discount")
+elif numT > 5 and numT <= 10:
+    print("20 % discount")
+elif numT > 10:
+    print("30 % discount")
+else:
+    print("incorrect input")
+
+# program 3
+# marks = 92
+# if  marks >90:
+#     print("Grade A")
+# if  marks >= 75:
+#     print("Grade B")
+# if marks >= 65:
+#     print("Grade C")
+
+# marks =17
+# if  marks >90:
+#     print("Grade A")
+# elif  marks >= 75:
+#     print("Grade B")
+# elif marks >= 65:
+#     print("Grade C")
+# else:
+#     print("Fail ...")
+    
+
+# s = 10 
+# t = 5 
+
+# if s > t:
+#     print("s is greater")
+# else:
+#     print('t is greater')
+
+# program 4
+x1 = 10
+x2 = 8
+x3 = 4
+
+if x1 > x2 and x1 > x3:
+    print("x1 is greater")
+elif x2 > x2 and x2 > x3:
+    print("x2 is greater")
+else:
+    print("x3 is greater")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
