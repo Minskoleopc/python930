be83b799ec0e26796826645cee0932a2d74ac5cc
unknown <Minskoleopc>
2024-01-19 21:51:40 +0530

    added

---
+++ script8.py
@@ -0,0 +1,60 @@
+# Gym()
+# Action -- excercise
+# Return -- health
+
+# def add(x,y):
+#     return "hello"
+# q = add(23,4)
+# print(q)
+
+# program 1 
+names = ["chinmay","sarika","poorva","samata"]
+q1 = len(names)
+print(q1)
+
+# program 2
+# e = names.append("ram")
+# print(names)
+# print(e)
+
+# names = ["chinmay","sarika","poorva","samata"]
+# names.clear()
+# print(names)
+
+# marks = [999,888,777,77,777,9999,777]
+# e2 = marks.count(777)
+# print(e2)
+
+#           0       1       2
+fruits = ["apple","mango","banana"]
+# fruits.extend(["grapes","papaya"])
+# print(fruits)
+
+fruits.insert(1,"chikoo")
+print(fruits)
+
+fruits.pop()
+print(fruits)
+
+fruits.pop(1)
+print(fruits)
+
+#                 0             1        2
+vegetables = ["cauliflower","potato","cabbage"]
+# vegetables.reverse()
+# print(vegetables)
+
+# vegetables.pop()
+# vegetables.pop(1)
+
+# vegetables.remove('cauliflower')
+# print(vegetables)
+
+# e2 = vegetables.index("potato")
+# print(e2)
+
+# vegetables.sort()
+# print(vegetables)
+
+v2 = vegetables.copy() # separate memory
+print(v2)
